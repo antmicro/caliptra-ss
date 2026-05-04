ce7a9b6feb5bd69e7276457354558b5b2fb888fb
Krystian Hebel <khebel@antmicro.com>
2026-05-04 16:40:23 +0200

    Disable RunThenComplete_M assertion
    See https://github.com/chipsalliance/caliptra-ss/issues/1115
    
    Signed-off-by: Krystian Hebel <khebel@antmicro.com>

--- src/integration/testbench/caliptra_ss_assertion_overrides.svh
+++ src/integration/testbench/caliptra_ss_assertion_overrides.svh
@@ -16,5 +16,9 @@
 //********************************************************************************
 
 initial begin
-//https://dev.azure.com/ms-tsd/SCIPS_IP/_workitems/edit/1196970    $assertoff(0, caliptra_ss_top_tb.caliptra_ss_dut.i3c.i3c.xhci.i3c_csr.ERR_HWIF_IN); // FIXME - remove https://github.com/chipsalliance/i3c-core/issues/22 
+//https://dev.azure.com/ms-tsd/SCIPS_IP/_workitems/edit/1196970    $assertoff(0, caliptra_ss_top_tb.caliptra_ss_dut.i3c.i3c.xhci.i3c_csr.ERR_HWIF_IN); // FIXME - remove https://github.com/chipsalliance/i3c-core/issues/22
+
+  // https://github.com/chipsalliance/caliptra-ss/issues/1115
+  // keccak_complete_i gets high during 13th clock cycle after keccak_run_o, but assertion expects it after 24th cycle
+  $assertoff(0, caliptra_ss_top_tb.caliptra_ss_dut.caliptra_top_dut.mldsa.sampler_top_inst.sha3_inst.u_pad.RunThenComplete_M);
 end
