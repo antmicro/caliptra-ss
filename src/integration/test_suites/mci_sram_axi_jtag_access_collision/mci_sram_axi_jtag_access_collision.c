c89cf61dbaef0ce223851b0e5bf979ee7fa6e0ab
Krystian Hebel <khebel@antmicro.com>
2026-06-22 18:00:55 +0200

    mci_sram_axi_jtag_access_collision: ensure DAI is idle before testing
    Signed-off-by: Krystian Hebel <khebel@antmicro.com>

--- src/integration/test_suites/mci_sram_axi_jtag_access_collision/mci_sram_axi_jtag_access_collision.c
+++ src/integration/test_suites/mci_sram_axi_jtag_access_collision/mci_sram_axi_jtag_access_collision.c
@@ -45,6 +45,7 @@ void main(void) {
     lcc_initialization();
     transition_state(TEST_UNLOCKED0, raw_unlock_token[0], raw_unlock_token[1], raw_unlock_token[2], raw_unlock_token[3], 1);
     reset_fc_lcc_rtl();
+    wait_dai_op_idle(0);
 
     // Wait for the SRAM to be unlocked
     while(!(lsu_read_32(SOC_SOC_IFC_REG_CPTRA_FLOW_STATUS) & 0x1));
